44ad4873cb429eb0d9c8fcb90c8f35f42023ffad
ajaybarath <ajaybarath3@gmail.com>
2021-11-11 22:40:12 +0530

    Added: payroll form ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import PayrollForm from './component/PayrollForm';
+import './component/payrollForm.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+
+    <header class="header-content header">
+        <div class="logo-content">
+            <img src="../assets/images/logo.png" alt="" />
+            <div>
+                <span class="emp-text">EMPLOYEE</span><br />
+                <span class="emp-text emp-payroll">PAYROLL</span>
+            </div>
+        </div>
+    </header>
+
+    <PayrollForm />
+
+</body>
   );
 }
 
