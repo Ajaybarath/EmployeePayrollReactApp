b403b720cab0b47418976ce596f452faa23a9029
ajaybarath <ajaybarath3@gmail.com>
2021-11-15 00:11:09 +0530

    Added: service to get data

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 import React from 'react';
 
 class EmployeeService extends React.Component {
-    baseUrl = `http://localhost:3000/employee`;
+    baseUrl = `http://localhost:3000/`;
 
     addEmployee = (data) => {
         console.log(data)
-        return axios.post(this.baseUrl, data)
+        return axios.post(this.baseUrl + 'employee', data)
     }
-}
+
+    getEmployee = () => {
+        console.log("getting data");
+        return axios.get(this.baseUrl + 'employee')
+    }
+ }
 
 export default new EmployeeService();
