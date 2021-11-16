8a40bb29b791f7ab518dc1119d510c11e3fff535
ajaybarath <ajaybarath3@gmail.com>
2021-11-16 09:29:17 +0530

    .Added: delete and update service

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -2,16 +2,26 @@ import axios from 'axios';
 import React from 'react';
 
 class EmployeeService extends React.Component {
-    baseUrl = `http://localhost:3000/`;
+    baseUrl = `http://localhost:8080/employeepayrollservice/`;
 
     addEmployee = (data) => {
         console.log(data)
-        return axios.post(this.baseUrl + 'employee', data)
+        return axios.post(this.baseUrl + 'create', data)
     }
 
     getEmployee = () => {
         console.log("getting data");
-        return axios.get(this.baseUrl + 'employee')
+        return axios.get(this.baseUrl + 'get')
+    }
+
+    updateEmployee = (data) => {
+        console.log(data);
+        return axios.put(this.baseUrl , data)
+    }
+
+    deleteEmployee = (data) => {
+        console.log(data);
+        return axios.delete(this.baseUrl + 'delete/' + data)
     }
  }
 
