a2b8e63a554f2721cb72c7d1ca6c46723fac25ad
ajaybarath <ajaybarath3@gmail.com>
2021-11-18 08:08:13 +0530

    Deleted: the comented code and log messages

--- src/component/homePage/HomePage.js
+++ src/component/homePage/HomePage.js
@@ -1,121 +1,115 @@
-import React, { Component, } from 'react'
+import React, { useState, useEffect } from 'react'
 import EmployeeService from '../../service/EmployeeService'
 import '../homePage/HomePage.css';
-import { Link } from 'react-router-dom';
-// import {useNavigate} from 'react-router-dom';
 import delBtn from '../assets/icons/delete-black-18dp.svg';
 import editBtn from '../assets/icons/create-black-18dp.svg';
 import addBtn from '../assets/icons/add-24px.svg';
-// import '../assets/profile-images/Ellipse 1.png';
-// import * as Images from '../assets/profile-images/';
+import PayrollForm from '../payroll/PayrollForm';
 
-class HomePage extends Component {
+import profile1 from '../assets/profile-images/Ellipse -3.png';
+import profile2 from '../assets/profile-images/Ellipse -1.png';
+import profile3 from '../assets/profile-images/Ellipse -8.png';
+import profile4 from '../assets/profile-images/Ellipse -7.png';
 
-    constructor(props) {
-        super(props)
+var imgUrl;
 
-        this.state = {
-            employees: [],
-            editStatus: ''
-        }
-
-        this.onEditClick = this.onEditClick.bind(this);
-        this.OnDelete = this.OnDelete.bind(this);
+var loadImages = (img) => {
+    if (img === '../assets/profile-images/Ellipse -1.png') {
+        imgUrl = profile2;
+    }
+    if (img === '../assets/profile-images/Ellipse -3.png') {
+        imgUrl = profile1;
+    }
+    if (img === '../assets/profile-images/Ellipse -8.png') {
+        imgUrl = profile3;
+    }
+    if (img === '../assets/profile-images/Ellipse -7.png') {
+        imgUrl = profile4;
     }
 
+};
 
-    onEditClick(event) {
-        // history = useHistory();
+function HomePage(props) {
 
-        // history.push('/add');
-        // const navigate = useNavigate();
-        // navigate('/home')
-        // console.log(this.props)
-        // this.props.navigation.navigate('/form', event.target.id)
+    const [employees, setEmployees] = useState([{}]);
+    const [editStatus, setEdit] = useState(false);
+    const [employeeToEdit, setEmployeeToEdit] = useState(null);
 
-
-    }
-
-    OnDelete(event) {
-        EmployeeService.deleteEmployee(event.target.id)
-            .then(res => {
-                EmployeeService.getEmployee()
-                    .then(res => {
-                        console.log(res.data.data);
-                        this.setState({ employees: res.data.data })
-                    })
-            })
-    }
-
-    componentDidMount() {
+    const fetchData = () => {
         EmployeeService.getEmployee()
             .then(res => {
-                console.log(res.data.data);
-                this.setState({ employees: res.data.data })
+                setEmployees({ employees: res.data.data })
             })
     }
 
-    render() {
-        // if (this.state.toDashboard) {
-        //     return <Redirect to='/dashboard' />
-        // }
-        return (
-            <div>
-                <div className="main-content">
-                    <div className="header-content">
-                        <div className="emp-detail-text">
-                            Employee Detail <div className="emp-content"></div>
-                        </div>
-                        <a href="/form" className="add-button">
-                            <img src={addBtn} alt="" />Add User</a>
-                    </div>
-
-                    <table className="table-main" id="display">
-
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Gender</th>
-                                <th>Department</th>
-                                <th>Salary</th>
-                                <th>Start Date</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        {this.state.employees.map((employee) => {
-                            return (<tr key={employee.id}>
-                                {/* {this.imagePath = require(employee.profileUrl+'')} */}
-                                <td>
-                                    <img src={employee.profileUrl} alt="" className="profile" />
-                                    {/* <Image style={{height: 50, width: 50}} source={this.imagePath} />
-                                       {console.log(this.imagePath)} */}
-                                </td>
-                                <td>{employee.name}</td>
-                                <td>{employee.gender}</td>
-                                <td>
-
-                                    {employee.department.map(dept => {
-
-                                        return <div className="dept-label" key={dept}>{dept}</div>
-                                    })}
-                                </td>
-                                <td>{employee.salary}</td>
-                                <td>{employee.startDate}</td>
-                                <td className="action-content">
-                                    <img src={delBtn} id={employee.id} onClick={this.OnDelete} alt="delete" />
-                                    <Link to='/form/' params={{employee:employee}} >
-                                        <img src={editBtn} id={employee.id} alt="delete" />
-                                    </Link>
-                                    {/* <img src={editBtn} id={employee.id} onClick={this.onEditClick} alt="delete" /> */}
-                                </td>
-                            </tr>)
-                        })}
-                    </table>
-                </div>
-            </div>
-        )
+    const OnDelete = (event) => {
+        EmployeeService.deleteEmployee(event.target.id)
+            .then(res => {
+                fetchData();
+            })
     }
+
+    const onEditClick = (event) => {
+        setEmployeeToEdit(event.target.id);
+        setEdit(true);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (editStatus)
+        return <PayrollForm employeeId={{ employeeToEdit }} />
+
+
+    return (
+        <div>
+            <div className="main-content">
+                <div className="header-content">
+                    <div className="emp-detail-text">
+                        Employee Detail <div className="emp-content"></div>
+                    </div>
+                    <a href="/form" className="add-button">
+                        <img src={addBtn} alt="" />Add User</a>
+                </div>
+
+                <table className="table-main" id="display">
+
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Gender</th>
+                        <th>Department</th>
+                        <th>Salary</th>
+                        <th>Start Date</th>
+                        <th>Actions</th>
+                    </tr>
+                    {employees.employees && employees.employees.map((employee) => {
+                        return (<tr key={employee.id}>
+                            <td>
+                                <img alt="" className="profile" onLoad={loadImages(employee.profilePic)} src={imgUrl} />
+                            </td>
+                            <td>{employee.name}</td>
+                            <td>{employee.gender}</td>
+                            <td>
+
+                                {employee.department.map(dept => {
+
+                                    return <div className="dept-label" key={dept}>{dept}</div>
+                                })}
+                            </td>
+                            <td>&#8377; {employee.salary}</td>
+                            <td>{employee.startDate}</td>
+                            <td className="action-content">
+                                <img src={delBtn} id={employee.id} onClick={OnDelete} alt="delete" />
+                                <img src={editBtn} id={employee.id} onClick={onEditClick} alt="delete" />
+                            </td>
+                        </tr>)
+                    })}
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
